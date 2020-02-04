68dbafcca825e9b600b90c107c5e30f80c9e6654
Piotr Kowalski <piecioshka@gmail.com>
2020-02-04 11:23:34 +0100

    Setup project

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const root = __dirname;
+
+module.exports = {
+    mode: "development",
+
+    devtool: "inline-source-map",
+
+    entry: path.join(root, 'src', 'index.js'),
+
+    output: {
+        filename: 'bundle.js',
+        path: path.join(root, 'dist')
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: "ts-loader" },
+            { test: /\.jsx?$/, loader: "babel-loader" }
+        ]
+    }
+};
