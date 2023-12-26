368105df6e968bf7b0962fc962a1bb2d56547fa2
Piotr Kowalski <piecioshka@gmail.com>
2023-12-26 16:56:19 +0100

    Remove usuless logs

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,4 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './components/app';
 
-console.time();
 createRoot(document.querySelector('#root')).render(<App />);
-console.timeEnd();
